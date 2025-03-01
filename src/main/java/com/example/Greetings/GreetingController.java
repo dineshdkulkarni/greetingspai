4d97f1b41a7d03de85d0825d9e2509364e2f35a3
dineshdkulkarni <dineshdkulkarni@gmail.com>
2025-03-01 17:55:04 +0530

    remove MySQL changes

--- src/main/java/com/example/Greetings/GreetingController.java
+++ src/main/java/com/example/Greetings/GreetingController.java
@@ -3,8 +3,12 @@ package com.example.Greetings;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.Greetings.models.GreetingRequest;
 
 @RestController
 public class GreetingController {
@@ -14,6 +18,12 @@ public class GreetingController {
 
 	@GetMapping("/greeting")
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
+
+	@PostMapping("/greeting")
+	public Greeting greeting(@RequestBody GreetingRequest request) {
+		String name = request.getName();
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
 }
--- src/main/java/com/example/Greetings/GreetingsApiApplication.java
+++ src/main/java/com/example/Greetings/GreetingsApiApplication.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GreetingsApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(GreetingsApplication.class, args);
-	}
-
+public class GreetingsApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GreetingsApiApplication.class, args);
+    }
 }
